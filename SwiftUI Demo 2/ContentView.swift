8cc769465eb9d14190ffe710df056e3988e74621
spencer1573 <spencer1573@users.noreply.github.com>
2019-10-26 19:02:23 -0600

    saving - even though there are errors

--- SwiftUI Demo 2/ContentView.swift
+++ SwiftUI Demo 2/ContentView.swift
@@ -10,17 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello Worlds")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }.padding()
+        VStack {
+            
+            MapView()
+            
+            VStack(alignment: .leading) {
+                Text("Hello Worlds")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }.padding()
+        }
     }
 }
 
--- SwiftUI Demo 2/MapView.swift
+++ SwiftUI Demo 2/MapView.swift
@@ -9,15 +9,18 @@
 import SwiftUI
 import MapKit
 
-struct MapView : UIViewRepresentable {
+struct MapView: UIViewRepresentable {
     
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
-        MKMapView()
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
     }
     
-    func updateUIView(_ uiView: MapView.UIViewType, context: UIViewRepresentableContext<MapView>) {
+    func updateUIView(_ view: MKMapView, context: Context) {
         
-        
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
     }
     
 }
